cbbfc8b5793221f22f5d4dfaaec520abddc02369
David McGinnis <dmcginnis427@gmail.com>
2020-03-04 14:20:42 +0100

    update on hc06

--- HC06-Setup/HC06-Setup.ino
+++ HC06-Setup/HC06-Setup.ino
@@ -21,7 +21,7 @@ char commandChar[32];
 void setup()
 {
   Serial.begin(9600);
-  Serial1.begin(9600);
+  Serial1.begin(115200);
   sendATCommand(at); 
 //  sendATCommand(deviceName);
 //  sendATCommand(password);
